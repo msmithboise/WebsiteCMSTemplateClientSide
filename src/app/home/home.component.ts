055d05637b2d0f269276fe316f7a533532d46d0e
Michael <msmithboise@gmail.com>
2020-07-11 15:36:02 -0600

    cleaned up code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,14 +17,13 @@ import { parse } from 'path';
 export class HomeComponent implements OnInit {
   WebContentArray: string[];
   newHeroImageUrl: string;
+  newHeader: string;
 
   constructor(public service: WebpageService) {}
 
   ngOnInit(): void {
     this.resetForm();
     this.service.getWebPageContent();
-    // this.asyncCall();
-    this.getImageCall();
   }
 
   resetForm(form?: NgForm) {
@@ -57,8 +56,8 @@ export class HomeComponent implements OnInit {
   }
 
   populateForm(item: Webpage) {
-    //this.service.formData = item;
-    this.service.formData = Object.assign({}, item);
+    this.service.formData = item;
+    //this.service.formData = Object.assign({}, item);
   }
 
   updateRecord(form: NgForm) {
@@ -74,51 +73,10 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  //need to set await before properties are assigned to myHeroImage
-
   myHeroImage = {
     height: '1000px',
     //   width: '1500px',
     'background-image': '',
     'background-attachment': 'fixed',
   };
-
-  async getImage() {
-    //saying this newHeroImageUrl value will be set to the response that service.getHeroImageUrl() retrieves
-    //'I am first (I get the data)');
-    var data = await this.service.getHeroImageUrl();
-    //console.log('Data before it is passed to assignVariable function:');
-    //console.log(data);
-
-    this.assignVariable(data);
-  }
-
-  assignVariable(data: Object) {
-    // console.log('I am second (I assign the data to a property)');
-    //console.log('Data after first being passed: ');
-    //console.log(data);
-
-    //console.log('Data after being stringified:');
-    var parsedData = JSON.parse(JSON.stringify(data));
-
-    // console.log('Attempting to access the array...');
-    //console.log(parsedData.HeroImageURL);
-
-    //console.log('Assigning variable...');
-    this.newHeroImageUrl = parsedData.HeroImageURL;
-  }
-  async getImageCall() {
-    await this.getImage();
-    console.log('Here is the image url: ');
-    console.log(this.newHeroImageUrl);
-
-    this.myHeroImage['background-image'] = this.newHeroImageUrl;
-    // this.myHeroImage['background-image'] =
-    //   'https://images.unsplash.com/photo-1537301696988-4a82a4959466?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-
-    console.log(this.myHeroImage['background-image']);
-
-    //'background-image': 'url(' + this.newHeroImageUrl + ')',
-    //might need to manipulate this string to remove "HeroImageURL": from it
-  }
 }
