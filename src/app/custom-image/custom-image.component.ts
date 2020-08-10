7ba34fdca037051ec0e31b64ce49cb6263df134b
Michael <msmithboise@gmail.com>
2020-08-10 00:48:57 -0600

    able to import image data

--- src/app/custom-image/custom-image.component.ts
+++ src/app/custom-image/custom-image.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomImageService } from '../services/custom-image.service';
+import { CustomImage } from '../models/custom-image.model';
 
 @Component({
   selector: 'app-custom-image',
   templateUrl: './custom-image.component.html',
-  styleUrls: ['./custom-image.component.css']
+  styleUrls: ['./custom-image.component.css'],
 })
 export class CustomImageComponent implements OnInit {
-
-  constructor() { }
+  constructor(public customImageService: CustomImageService) {}
 
   ngOnInit(): void {
+    this.callCustomImageService();
   }
 
+  callCustomImageService() {
+    this.customImageService
+      .getCustomImageContent()
+      .subscribe((res: CustomImage[]) => {
+        this.customImageService.customImageArray = res;
+        console.log('here is your custom image array: ');
+        console.log(this.customImageService.customImageArray);
+      });
+  }
 }
--- src/app/services/custom-image.service.ts
+++ src/app/services/custom-image.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { CustomImage } from '../models/custom-image.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomImageService {
+  readonly webApi = 'http://localhost:54704/api';
+  public customImageArray: CustomImage[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  //Get Image array
+  getCustomImageContent(): Observable<CustomImage[]> {
+    return this.http.get<CustomImage[]>(this.webApi + '/CustomImages');
+  }
 }
