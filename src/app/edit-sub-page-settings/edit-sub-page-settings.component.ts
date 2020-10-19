62feebf6e84534f13efa059fa03bc60639307876
Michael <msmithboise@gmail.com>
2020-10-19 11:48:25 -0600

    grabbing page id

--- src/app/edit-sub-page-settings/edit-sub-page-settings.component.ts
+++ src/app/edit-sub-page-settings/edit-sub-page-settings.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { Subpage } from '../models/subpage.model';
@@ -12,18 +14,34 @@ import { SubpageService } from '../services/subpage.service';
 })
 export class EditSubPageSettingsComponent implements OnInit {
   pageDescription: string;
+  pageId: number;
+
   selectedDescription;
   constructor(
     public subPageService: SubpageService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public toastr: ToastrService
   ) {}
 
   ngOnInit() {
     this.pageDescription = this.route.snapshot.paramMap.get('pageDescription');
     console.log(this.pageDescription);
+    this.pageId = Number(this.route.snapshot.paramMap.get('pageId'));
+    console.log(this.pageId);
   }
 
-  getSubPages(pageId: number, pageDescription: string) {
+  addNewSubPage(form: NgForm) {
+    this.subPageService
+      .createSubPage(form.value)
+      .subscribe((res: Subpage[]) => {
+        this.subPageService.subPageArray = res;
+        this.getSubPages();
+        this.toastr.success('Page created!');
+        console.log('is this being calleD?');
+      });
+  }
+
+  getSubPages() {
     this.subPageService.getSubPages().subscribe((res: Subpage[]) => {
       this.subPageService.subPageArray = res;
 
--- src/app/services/subpage.service.ts
+++ src/app/services/subpage.service.ts
@@ -9,11 +9,19 @@ import { Subpage } from '../models/subpage.model';
 export class SubpageService {
   readonly webApi = 'http://localhost:54704/api';
   subPageArray: Subpage[];
+  public subPageFormData: Subpage;
 
   constructor(public http: HttpClient) {}
 
   //Get
   getSubPages(): Observable<Subpage[]> {
     return this.http.get<Subpage[]>(this.webApi + '/SubPages');
+  }
+
+  //Post/Put
+
+  //post
+  createSubPage(formData: Subpage) {
+    return this.http.post(this.webApi + '/SubPages', formData);
   }
 }
