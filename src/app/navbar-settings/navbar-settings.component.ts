9125b7f0ea0d68045fa2c649c42a30096580c71a
Michael <msmithboise@gmail.com>
2020-10-27 11:42:12 -0600

    navbar settings setup

--- src/app/navbar-settings/navbar-settings.component.ts
+++ src/app/navbar-settings/navbar-settings.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { NavBarService } from '../services/nav-bar.service';
 
 @Component({
   selector: 'app-navbar-settings',
   templateUrl: './navbar-settings.component.html',
-  styleUrls: ['./navbar-settings.component.css']
+  styleUrls: ['./navbar-settings.component.css'],
 })
 export class NavbarSettingsComponent implements OnInit {
+  constructor(
+    public navBarService: NavBarService,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onEditSubmit(form: NgForm) {
+    this.insertEditSettings(form);
   }
 
+  insertEditSettings(form: NgForm) {
+    console.log('form and id:');
+    console.log(form);
+
+    // this.route.params.subscribe((params) => {
+    //   this.pageId = params.pageId;
+    //   this.pageDescription = params.pageDescription;
+    // });
+  }
 }
