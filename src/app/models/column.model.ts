d98e77e6693cbd4bf5d3a4fb1896abf3aaf87b22
Michael <msmithboise@gmail.com>
2020-11-06 16:55:02 -0700

    added column model

---
+++ src/app/models/column.model.ts
@@ -0,0 +1,11 @@
+export class Column {
+  ColumnId: number;
+  ColumnClass: string;
+  RowId: number;
+  RowClass: string;
+  PageId: number;
+  PageDescription: string;
+  ElementTagId: number;
+  ElementTag: string;
+  Id: number;
+}
--- src/app/web-structure.service.ts
+++ src/app/web-structure.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Column } from './models/column.model';
 import { Row } from './models/row.model';
 
 @Injectable({
@@ -26,5 +27,21 @@ export class WebStructureService {
   //Post rows
   postRowsByPageId(formData: FormGroup) {
     return this.http.post(this.webApi + '/Row', formData);
+  }
+
+  //Get Columns
+  getColumns() {
+    return this.http.get<Column[]>(this.webApi + '/Columns');
+  }
+
+  //Get Columns by PageId (and RowId)
+
+  getColumnsByPageId(pageId: number) {
+    return this.http.get<Column[]>(this.webApi + '/Columns/' + pageId);
+  }
+
+  //Post Columns
+  postColumnsByPageId(formData: FormGroup) {
+    return this.http.post(this.webApi + '/Columns', formData);
   }
 }
