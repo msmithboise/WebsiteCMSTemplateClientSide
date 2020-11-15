0a7ff928b06a094b263fea1ed3825dc3dddbe2ff
Michael <msmithboise@gmail.com>
2020-11-14 22:01:06 -0700

    added import to button panel

--- src/app/button-panel/button-panel.component.ts
+++ src/app/button-panel/button-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { WebcontentService } from '../WebContent/webcontent.service';
 
@@ -8,23 +8,27 @@ import { WebcontentService } from '../WebContent/webcontent.service';
   styleUrls: ['./button-panel.component.css'],
 })
 export class ButtonPanelComponent implements OnInit {
+  @Input() columnId: number;
   constructor(public webContentService: WebcontentService) {}
 
   ngOnInit(): void {}
 
   //To submit text body data
   submitNewTextData(form: FormGroup) {
+    console.log('columnId', this.columnId);
     this.insertTextRecord(form);
   }
 
   textFormTemplate = new FormGroup({
     TextBody: new FormControl('', Validators.required),
     pageId: new FormControl(''),
+    columnId: new FormControl(''),
   });
 
   insertTextRecord(form: FormGroup) {
     var newForm = this.textFormTemplate.value;
     newForm.pageId = this.webContentService.pageIdSnapshot;
+    newForm.columnId = this.columnId;
 
     this.webContentService.postWebContentByPageId(newForm).subscribe((res) => {
       //this.resetForm(form);
