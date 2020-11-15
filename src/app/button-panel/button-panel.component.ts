4f3bbe18dc98c6ab37726084dc4943d456ead4ad
Michael <msmithboise@gmail.com>
2020-11-14 22:30:27 -0700

    added image to panel

--- src/app/button-panel/button-panel.component.ts
+++ src/app/button-panel/button-panel.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { WebcontentService } from '../WebContent/webcontent.service';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-button-panel',
@@ -9,9 +12,50 @@ import { WebcontentService } from '../WebContent/webcontent.service';
 })
 export class ButtonPanelComponent implements OnInit {
   @Input() columnId: number;
-  constructor(public webContentService: WebcontentService) {}
+  public rowId = '';
+  closeResult = '';
+  public fileToUpload: File = null;
+  imgSrc: string;
+  audioSrc: string;
+  selectedImage: any = null;
+  selectedAudio: any = null;
+  isSubmitted: boolean;
+  isAudioSubmitted: boolean;
+  imageList: any[];
+  fireBaseImageUrl: string;
+  resizeButtonToggled: boolean = false;
+  selectedId: number;
+  public pageDescription: string;
+  public pageId: number;
+  constructor(
+    public webContentService: WebcontentService,
+    private storage: AngularFireStorage,
+    public toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
+
+  showPreview(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => (this.audioSrc = e.target.result);
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    } else {
+      this.audioSrc = '/assets/placeholder.jpg';
+      this.selectedImage = null;
+    }
+  }
+
+  formTemplate = new FormGroup({
+    imageUrl: new FormControl('', Validators.required),
+    pageId: new FormControl(''),
+    backgroundImage: new FormControl(''),
+  });
+
+  get formControls() {
+    return this.formTemplate['controls'];
+  }
 
   //To submit text body data
   submitNewTextData(form: FormGroup) {
@@ -34,5 +78,77 @@ export class ButtonPanelComponent implements OnInit {
       //this.resetForm(form);
       //this.grabAllContentByPageId();
     });
+  }
+
+  resetForm() {
+    this.formTemplate.reset();
+    this.formTemplate.setValue({
+      imageUrl: '',
+      pageId: 0,
+      backgroundImage: '',
+    });
+    this.imgSrc = '/assets/placeholder.jpg';
+    this.selectedImage = null;
+    this.isSubmitted = false;
+  }
+
+  onSubmit(formValue) {
+    this.isSubmitted = true;
+    if (this.formTemplate.valid) {
+      var filePath = `images/${this.selectedImage.name
+        .split('.')
+        .slice(0, -1)
+        .join('.')}_${new Date().getTime()}`;
+      const fileRef = this.storage.ref(filePath);
+      this.storage
+        .upload(filePath, this.selectedImage)
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            fileRef.getDownloadURL().subscribe((url) => {
+              this.webContentService
+                .postUploadedImage(url)
+                .subscribe((data) => {});
+
+              formValue['imageUrl'] = url;
+
+              //this.webContentService.insertImageDetails(formValue);
+              this.resetForm();
+            });
+          })
+        )
+        .subscribe((res) => {
+          //this.grabAllContentByPageId();
+        });
+      this.toastr.success('Image uploaded succesfully!');
+    }
+  }
+
+  //To submit image url
+
+  imageUrlFormTemplate = new FormGroup({
+    imageUrl: new FormControl('', Validators.required),
+    pageId: new FormControl(''),
+  });
+
+  submitImageUrlData(form: FormGroup) {
+    // console.log('image form after submit');
+    // console.log(form);
+    this.insertImageUrlRecord(form);
+  }
+
+  insertImageUrlRecord(form: FormGroup) {
+    // console.log('image form during insert record');
+    // console.log(form);
+
+    var newUrlForm = this.imageUrlFormTemplate.value;
+    newUrlForm.pageId = this.webContentService.pageIdSnapshot;
+
+    this.webContentService
+      .postWebContentByPageId(newUrlForm)
+      .subscribe((res) => {
+        //this.resetForm(form);
+        //this.grabAllContentByPageId();
+      });
   }
 }
--- src/app/page-settings/page-settings.component.ts
+++ src/app/page-settings/page-settings.component.ts
@@ -216,18 +216,6 @@ export class PageSettingsComponent implements OnInit {
       });
   }
 
-  showPreview(event: any) {
-    if (event.target.files && event.target.files[0]) {
-      const reader = new FileReader();
-      reader.onload = (e: any) => (this.audioSrc = e.target.result);
-      reader.readAsDataURL(event.target.files[0]);
-      this.selectedImage = event.target.files[0];
-    } else {
-      this.audioSrc = '/assets/placeholder.jpg';
-      this.selectedImage = null;
-    }
-  }
-
   showAudioPreview(event: any) {
     // console.log('show audio preview');
 
@@ -242,21 +230,6 @@ export class PageSettingsComponent implements OnInit {
     }
   }
 
-  get formControls() {
-    return this.formTemplate['controls'];
-  }
-
-  formTemplate = new FormGroup({
-    imageUrl: new FormControl('', Validators.required),
-    pageId: new FormControl(''),
-    backgroundImage: new FormControl(''),
-  });
-
-  imageUrlFormTemplate = new FormGroup({
-    imageUrl: new FormControl('', Validators.required),
-    pageId: new FormControl(''),
-  });
-
   embedUrlFormTemplate = new FormGroup({
     embedUrl: new FormControl('', Validators.required),
     pageId: new FormControl(''),
@@ -267,50 +240,6 @@ export class PageSettingsComponent implements OnInit {
     pageId: new FormControl(''),
     backgroundColor: new FormControl(''),
   });
-
-  resetForm() {
-    this.formTemplate.reset();
-    this.formTemplate.setValue({
-      imageUrl: '',
-      pageId: 0,
-      backgroundImage: '',
-    });
-    this.imgSrc = '/assets/placeholder.jpg';
-    this.selectedImage = null;
-    this.isSubmitted = false;
-  }
-
-  onSubmit(formValue) {
-    this.isSubmitted = true;
-    if (this.formTemplate.valid) {
-      var filePath = `images/${this.selectedImage.name
-        .split('.')
-        .slice(0, -1)
-        .join('.')}_${new Date().getTime()}`;
-      const fileRef = this.storage.ref(filePath);
-      this.storage
-        .upload(filePath, this.selectedImage)
-        .snapshotChanges()
-        .pipe(
-          finalize(() => {
-            fileRef.getDownloadURL().subscribe((url) => {
-              this.webContentService
-                .postUploadedImage(url)
-                .subscribe((data) => {});
-
-              formValue['imageUrl'] = url;
-
-              //this.webContentService.insertImageDetails(formValue);
-              this.resetForm();
-            });
-          })
-        )
-        .subscribe((res) => {
-          this.grabAllContentByPageId();
-        });
-      this.toastr.success('Image uploaded succesfully!');
-    }
-  }
 
   //TO submit link data
 
@@ -336,29 +265,6 @@ export class PageSettingsComponent implements OnInit {
       //this.resetForm(form);
       this.grabAllContentByPageId();
     });
-  }
-
-  //To submit image url
-
-  submitImageUrlData(form: FormGroup) {
-    // console.log('image form after submit');
-    // console.log(form);
-    this.insertImageUrlRecord(form);
-  }
-
-  insertImageUrlRecord(form: FormGroup) {
-    // console.log('image form during insert record');
-    // console.log(form);
-
-    var newUrlForm = this.imageUrlFormTemplate.value;
-    newUrlForm.pageId = this.webContentService.pageIdSnapshot;
-
-    this.webContentService
-      .postWebContentByPageId(newUrlForm)
-      .subscribe((res) => {
-        //this.resetForm(form);
-        this.grabAllContentByPageId();
-      });
   }
 
   //To embed image url
@@ -526,6 +432,24 @@ export class PageSettingsComponent implements OnInit {
     if (confirm('Are you sure you want to delete this?')) {
       this.onDelete(id);
     }
+  }
+
+  formTemplate = new FormGroup({
+    imageUrl: new FormControl('', Validators.required),
+    pageId: new FormControl(''),
+    backgroundImage: new FormControl(''),
+  });
+
+  resetForm() {
+    this.formTemplate.reset();
+    this.formTemplate.setValue({
+      imageUrl: '',
+      pageId: 0,
+      backgroundImage: '',
+    });
+    this.imgSrc = '/assets/placeholder.jpg';
+    this.selectedImage = null;
+    this.isSubmitted = false;
   }
 
   onDelete(id: number) {
