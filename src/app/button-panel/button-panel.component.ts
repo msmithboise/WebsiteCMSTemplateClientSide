79617c77ea4bcab468222419e5d56d679b06beff
Michael <msmithboise@gmail.com>
2021-02-19 11:00:02 -0700

    button can be created no styles yet

--- src/app/button-panel/button-panel.component.ts
+++ src/app/button-panel/button-panel.component.ts
@@ -463,7 +463,7 @@ export class ButtonPanelComponent implements OnInit {
 
   // To create button
   buttonFormTemplate = new FormGroup({
-    buttonText: new FormControl('', Validators.required),
+    buttonText: new FormControl(''),
     pageId: new FormControl(''),
     columnId: new FormControl(''),
   });
