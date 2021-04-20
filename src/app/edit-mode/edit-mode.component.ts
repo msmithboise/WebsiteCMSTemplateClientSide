fbd5fd051ead58b01129ed9463bec28d01e778f4
Michael <msmithboise@gmail.com>
2021-04-19 19:32:10 -0600

    2nd drag container works

--- src/app/edit-mode/edit-mode.component.ts
+++ src/app/edit-mode/edit-mode.component.ts
@@ -1,24 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DragulaService } from 'ng2-dragula';
+
+import autoScroll from 'dom-autoscroller';
+import * as dragula from 'dragula';
 
 @Component({
   selector: 'app-edit-mode',
   templateUrl: './edit-mode.component.html',
   styleUrls: ['./edit-mode.component.css'],
 })
+
+//@ViewChild('autoscroll') autoscroll: ElementRef;
 export class EditModeComponent implements OnInit {
   public characters = [
-    { id: 1, firstName: `Jeffry`, lastName: 'Houser' },
-    { id: 2, firstName: `Annie`, lastName: 'Anderson' },
-    { id: 3, firstName: `Bob`, lastName: 'Boberson' },
-    { id: 4, firstName: `Candy`, lastName: 'Canderson' },
-    { id: 5, firstName: `Davey`, lastName: 'Daverson' },
+    { id: 1, firstName: `Text` },
+    { id: 2, firstName: `Background Image` },
+    { id: 3, firstName: `Button` },
+    { id: 4, firstName: `Audio` },
+    { id: 5, firstName: `Video` },
   ];
+
+  selectedCharacters = [];
 
   constructor(private dragulaService: DragulaService) {}
 
   ngOnInit(): void {
     this.createDragulaGroup();
+    this.scroll();
+  }
+
+  scroll() {
+    var drake = dragula([
+      document.querySelector('#list'),
+      document.querySelector('#hlist'),
+    ]);
+
+    var scroll = autoScroll(
+      [
+        window,
+        document.querySelector('#list-container'),
+        document.querySelector('#container2'),
+      ],
+      {
+        margin: 20,
+        autoScroll: function () {
+          return this.down && drake.dragging;
+        },
+      }
+    );
   }
 
   createDragulaGroup() {
